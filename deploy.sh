8c57ff2c713a57cc20b3f23bc8200b5e76a14b08
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-06-26 15:19:30 +0100

    made changes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 git pull
-pm2 stop server
+rm -r node_modules/
 npm install
 npm prisma migrate deploy
 node ace build
 cd build/
-npm ci --omit="dev"   
+npm ci --omit="dev"  
+pm2 stop server 
 ENV_PATH=$ENV_PATH pm2 start bin/server.js
