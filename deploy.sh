3b2f2433f09a4949f308c1f308b569c451fed6d6
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-06-26 14:34:22 +0100

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 git pull
 pm2 stop server
+npm install
+npm prisma migrate deploy
 node ace build
-ENV_PATH=.env pm2 start build/bin/server.js
+cd build/
+
+ENV_PATH=$ENV_PATH pm2 start build/bin/server.js
