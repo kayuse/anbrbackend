dad5c211a237199a8acc0a1ee7eceaacdf1414b1
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-06-26 15:37:24 +0100

    sss

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ node ace build
 cd build/
 npm ci --omit="dev"  
 pm2 stop server 
-ENV_PATH=$ENV_PATH pm2 start bin/server.js
+# ENV_PATH=$ENV_PATH pm2 start bin/server.js
+cp ../.env .env
+pm2 start bin/server.js
