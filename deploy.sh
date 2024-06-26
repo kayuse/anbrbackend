046ba435cca8afeacd8b4e6923ab926339aebc81
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-06-26 15:43:42 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ npm prisma migrate deploy
 node ace build
 cd build/
 npm ci --omit="dev"  
+cd ..
 pm2 stop server 
-# ENV_PATH=$ENV_PATH pm2 start bin/server.js
-cp ../.env .env
+ENV_PATH=$ENV_PATH pm2 start bin/server.js
+# cp ../.env .env
 pm2 start bin/server.js
