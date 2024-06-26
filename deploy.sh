07c46355368dbebb3de08118a2284015d9ccc0ff
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-06-26 16:22:14 +0100

    mkk

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,8 @@ pm2 stop server
 # ENV_PATH=$ENV_PATH pm2 start bin/server.js
 cp ../.env .env
 pm2 start bin/server.js
+
+
+#   // "optionalDependencies": {
+#   //      "@rollup/rollup-linux-x64-gnu": "4.9.5"
+#   // },
